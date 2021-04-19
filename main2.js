4dbdb1ff4b9f9914258309d691fa9d38ffa11d80
hknktk <75674444+hknktk@users.noreply.github.com>
2021-04-19 22:27:25 +0900

    commit
    更新した。

--- main2.js
+++ main2.js
@@ -5,92 +5,102 @@
   const $set_button = document.querySelector('#set_button');
   const $copy_button = document.querySelector('#copy_button');
   const $alert_message = document.querySelector('#alert');
-
-
-  function countUp() {
-    // $text.value= $text.value.replace(/\r\n/g, '');
-    // $text.value = $text.value.replace(/\n/g, '');
-    // $text.value = $text.value.replace(/\s+/g, '');
-    let $length = $text.value.length;
-    $number.textContent = $length;
-    // alert('countUp');
-  }
-
-  $counter_button.addEventListener('click', () =>{
-    countUp();
-    // countDown();
-  })
+  const $result = document.querySelector('#result');
 
 
 
-  
-  function textCopy() {
-    // let textTarget = document.querySelector('#content');
-    // textTarget.select();
-    // document.execCommand('Copy');
-    alert('copy')
-  }
-
-  function countDown() {
-    // let $length = $text.value.length;
-    // let $setting_length = document.querySelector('#setting_length').value;
-
-    // if($setting_length === ''){
-    //   return;
-    // }
-
-    // else if($setting_length - $length < 0){
-    //   $alert_message.textContent = $length-$setting_length + "文字オーバーしています。";
-    // }
-    // else if($setting_length - $length === 0){
-    //   $alert_message.textContent = 'ちょうど'+ $setting_length + '文字です。';
-    // }else{
-    //   $alert_message.textContent = $setting_length - $length + '文字足りません。';
-    // }
-    // alert('countDown');
-  }
-
-  function alertMessageShow(){
-    // $alert_message.classList.add('show');
-    alert('alertmessageShow');
-  } 
 
   function numberCheck() {
-    // let $setting_length = document.querySelector('#setting_length').value;
-    // if(!isNaN($setting_length)){
-    //   countDown();
-    //   alertMessageShow();
-    // }else{
-    //   $alert_message.textContent = '数字以外が入力されています。';
-    //   alertMessageShow();
-    // }
-    alert('numberChecked');
+    let $setting_length = document.querySelector('#setting_length').value;
+    if(!isNaN($setting_length)){
+      let $length = $text.value.length;
+      let $setting_length = document.querySelector('#setting_length').value;
+  
+      if($setting_length === ''){
+        return;
+      }
+  
+      else if($setting_length - $length < 0){
+        $alert_message.textContent = $length-$setting_length + "文字オーバーしています。";
+      }
+      else if($setting_length - $length === 0){
+        $alert_message.textContent = 'ちょうど'+ $setting_length + '文字です。';
+      }else{
+        $alert_message.textContent = $setting_length - $length + '文字足りません。';
+      }
+      $alert_message.classList.add('show');
+    }else{
+      $alert_message.textContent = '数字以外が入力されています。';
+      $alert_message.classList.add('show');
+    }
   }
 
 
   $set_button.addEventListener('click', () =>{
-      numberCheck();
+    let $setting_length = document.querySelector('#setting_length').value;
+    if(!isNaN($setting_length)){
+      let $length = $text.value.length;
+      let $setting_length = document.querySelector('#setting_length').value;
+  
+      if($setting_length === ''){
+        return;
+      }
+  
+      else if($setting_length - $length < 0){
+        $alert_message.textContent = $length-$setting_length + "文字オーバーしています。";
+      }
+      else if($setting_length - $length === 0){
+        $alert_message.textContent = 'ちょうど'+ $setting_length + '文字です。';
+      }else{
+        $alert_message.textContent = $setting_length - $length + '文字足りません。';
+      }
+      $alert_message.classList.add('show');
+    }else{
+      $alert_message.textContent = '数字以外が入力されています。';
+      $alert_message.classList.add('show');
+    }
   })
 
 
   
+  $counter_button.addEventListener('click', () =>{
+    $text.value= $text.value.replace(/\r\n/g, '');
+    $text.value = $text.value.replace(/\n/g, '');
+    $text.value = $text.value.replace(/\s+/g, '');
+    let $length = $text.value.length;
+    $number.textContent = $length;
+    let $setting_length = document.querySelector('#setting_length').value;
+
+    if($setting_length === ''){
+      return;
+    }
+
+    else if($setting_length - $length < 0){
+      $alert_message.textContent = $length-$setting_length + "文字オーバーしています。";
+    }
+    else if($setting_length - $length === 0){
+      $alert_message.textContent = 'ちょうど'+ $setting_length + '文字です。';
+    }else{
+      $alert_message.textContent = $setting_length - $length + '文字足りません。';
+    }
+  })
+  
 
 
   $copy_button.addEventListener('click', () => {
-    textCopy();
-    result_display();
+    let textTarget = document.querySelector('#content');
+    textTarget.select();
+    document.execCommand('Copy');
+
+    $result.classList.add('show');
+
+    setTimeout(() => {
+    $result.classList.remove('show');
+    },6000);
   })
 
 
-  const $result = document.querySelector('#result');
-  function result_display() {
-    // $result.classList.add('show');
 
-    // setTimeout(() => {
-    // $result.classList.remove('show');
-    // },6000);
-    alert('result_display');
-  }
 
 
 }
