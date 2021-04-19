477abe19c30287755f2bc6de89d930ab3b878992
hknktk <75674444+hknktk@users.noreply.github.com>
2021-04-19 16:13:32 +0900

    commit
    更新した。

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 {
   const $text = document.querySelector('#content');
+  const $minus = document.querySelector('#minus');
   const $number = document.querySelector('#counter_number');
+  const $counter_button = document.querySelector('#counter_button');
+  const $set_button = document.querySelector('#set_button');
   const $copy_button = document.querySelector('#copy_button');
+  const $alert_message = document.querySelector('#alert');
 
 
   function countUp() {
@@ -12,21 +16,64 @@
     $number.textContent = $length;
   }
 
-  $text.addEventListener('keyup', () =>{
+
+
+  
+  function textCopy() {
+    let textTarget = document.querySelector('#content');
+    textTarget.select();
+    document.execCommand('Copy');
+  }
+
+  function countDown() {
+    let $length = $text.value.length;
+    let $setting_length = document.querySelector('#setting_length').value;
+    $alert_message.textContent = $setting_length - $length + '文字足りません。';
+    if($setting_length - $length < 0){
+      $alert_message.textContent = $length-$setting_length + "文字オーバーしています。"
+    }
+  }
+
+  function alertMessageShow(){
+    $alert_message.classList.add('show');
+  } 
+
+  function numberCheck() {
+    let $setting_length = document.querySelector('#setting_length').value;
+    if(!isNaN($setting_length)){
+      countDown();
+      alertMessageShow();
+    }else{
+      $alert_message.textContent = '数字以外が入力されています。';
+      alertMessageShow();
+    }
+  }
+
+
+  $set_button.addEventListener('click', () =>{
+    numberCheck();
+  })
+
+
+  $counter_button.addEventListener('click', () =>{
     countUp();
+    countDown();
   })
 
 
   $copy_button.addEventListener('click', () => {
     textCopy();
+    result_display();
   })
 
-  function textCopy() {
-    let textTarget = document.querySelector('#content');
-    textTarget.select();
-    document.execCommand('Copy');
 
-    alert('コピーが完了しました。');
+  const $result = document.querySelector('#result');
+  function result_display() {
+    $result.classList.add('show');
+
+    setTimeout(() => {
+    $result.classList.remove('show');
+    },6000);
   }
 
 
