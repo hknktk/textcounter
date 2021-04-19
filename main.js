cfb0f15ccd5372ca4deed17f500a785a978f82a2
hknktk <75674444+hknktk@users.noreply.github.com>
2021-04-19 11:08:38 +0900

    commit
    更新した。

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 {
   const $text = document.querySelector('#content');
   const $number = document.querySelector('#counter_number');
-  const $button = document.querySelector('#button');
+  const $counter_button = document.querySelector('#counter_button');
+  const $copy_button = document.querySelector('#copy_button');
 
 
-
-  $button.addEventListener('click' , () =>{
+  function countUp() {
     $text.value= $text.value.replace(/\r\n/g, '');
     $text.value = $text.value.replace(/\n/g, '');
     $text.value = $text.value.replace(/\s+/g, '');
     let $length = $text.value.length;
     $number.textContent = $length;
+  }
+
+  $counter_button.addEventListener('click' , () =>{
+    countUp();
   })
+
+
+  $copy_button.addEventListener('click' , () => {
+    textCopy();
+  })
+
+  function textCopy() {
+    let textTarget = document.querySelector('#content');
+    textTarget.select();
+    document.execCommand('Copy');
+
+    alert('コピーが完了しました。');
+  }
+
 
 }
